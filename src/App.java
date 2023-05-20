6a1e4a1363ce9297bd2b29772966c21a18bab941
21 September <Thammanit.ch@gmail.com>
2023-05-20 21:21:54 +0700

    update file r/w

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,11 +15,14 @@ public class App {
         Key key = keymode.equals("-gen") ? generateKey() : new Key(args[3]); // get key ready for process
         String textOrFilePath = null;
         outputFilePath = args[args.length - 1];
+        if(fileExists(outputFilePath)) {
+            writeEmptyFile();
+        }
         // text mode
         if (mode.equals("-t")) {
             // read rest of string input by args index
             textOrFilePath = readAllString(keymode, args);
-            System.out.println(textOrFilePath);
+            System.out.println("Working on: " + textOrFilePath);
             // string encryption
             if (encryptOrDecrypt.equals("-e")) {
                 encryptText(textOrFilePath, key);
@@ -60,7 +64,6 @@ public class App {
         } else {
             System.out.println("Invalid input from command line");
         }
-        // System.out.println("Output file: " + outputFilePath);
     }
 
     // <<----------------- END OF MAIN ----------------->>
@@ -76,10 +79,19 @@ public class App {
         return Files.readAllBytes(path);
     }
 
-    public static void writeToFile(byte[] data) throws IOException {
+    public static void writeEmptyFile() throws IOException {
         FileWriter writer = new FileWriter(outputFilePath);
-        writer.write(data.toString());
         writer.close();
+    }
+
+    //case text mode
+    public static void writeByteToFile(byte[] data) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(outputFilePath, true)) {
+            fos.write(data);
+            System.out.println("Data has been written to the file.");
+        } catch (IOException e) {
+            System.err.println("An error occurred while writing to the file: " + e.getMessage());
+        }
     }
 
     // <<----------------- END OF FILE OPERATION ----------------->>
@@ -106,23 +118,30 @@ public class App {
     // <<----------------- KEY OPERATION ----------------->>
     public static Key generateKey() {
         Key key = new Key();
-        key.generateKey();
         return key;
     }
 
     // <<----------------- END OF KEY OPERATION ----------------->>
     // ----------------------------------------------------------------
     // <<----------------- ENCRYPT AND DECRYPT ----------------->>
+    //case user input text from command line
     public static void encryptText(String text, Key key) throws IOException {
         // encryption process
-        System.out.println("Encrypting text: " + text + " with key: " + key.getKey());
-        writeToFile(text.getBytes());
+        System.out.println("Encrypting message...");
+        /*
+         * ENCRYPTION IMLPEMENTATION
+         */
+        writeByteToFile(text.getBytes()/* byte array after encrypt */);
     };
 
+    //case user input text from command line
     public static void decryptText(String text, Key key) throws IOException {
         // decryption process
-        System.out.println("Decrypting text: " + text + " with key: " + key.getKey());
-        writeToFile(text.getBytes());
+        System.out.println("Decrypting message...");
+        /*
+         * DECRYPTION IMLPEMENTATION
+         */
+        writeByteToFile(text.getBytes() /* byte array after decrypt */);
     };
 
     public static void encryptFile(String filePath, Key key) throws IOException {
@@ -132,9 +151,18 @@ public class App {
         } catch (IOException e) {
             System.err.println("Error reading file: " + e.getMessage());
         }
-        // encryption process
-        System.out.println("Encrypting file: " + fileBytes + " with key: " + key.getKey());
-        writeToFile(fileBytes);
+        /*
+         * ENCRYPTION IMLPEMENTATION
+         */
+        System.out.println("Encrypting file: " + filePath);
+        // System.out.println("\nFILE BYTE WRITE");
+        // writeTextToFile("file", filePath);
+        writeByteToFile(fileBytes /* byte array after encrypt */);
+        // System.out.println("----------------------------------------\n");
+        // System.out.println("\nKEY BYTE WRITE");
+        // writeTextToFile("key", key.getKey().toString());
+        writeByteToFile(key.getKey());
+
     };
 
     public static void decryptFile(String filePath, Key key) throws IOException {
@@ -144,9 +172,14 @@ public class App {
         } catch (IOException e) {
             System.err.println("Error reading file: " + e.getMessage());
         }
-        // decryption process
-        System.out.println("Decrypting file: " + fileBytes + " with key: " + key.getKey());
-        writeToFile(fileBytes);
+        /*
+         * DECRYPTION IMLPEMENTATION
+         */
+        System.out.println("Decrypting file: " + filePath);
+        // writeTextToFile("file", filePath);
+        writeByteToFile(fileBytes /* byte array after decrypt */);
+        // writeTextToFile(key.getKey().toString(), "key");
+        writeByteToFile(key.getKey());
     };
     // <<----------------- END OF ENCRYPT AND DECRYPT ----------------->>
 }
